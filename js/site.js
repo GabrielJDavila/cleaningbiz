034f1127852e54f754d7a7aa0a24f1efbdb93ced
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2022-08-16 21:47:26 -0500

    Add files via upload
    added some button functions to home page

--- js/site.js
+++ js/site.js
@@ -4,8 +4,12 @@ const menuIcon = document.querySelector('.material-symbols-outlined');
 const dropDownMenu = document.querySelector('nav')
 const scrollDownBtn = document.querySelector('.scroll-down')
 const reviewSection = document.querySelector('.reviews-section')
-
-
+const learnMoreBtn1 = document.querySelector('.sl-1');
+const learnMoreBtn2 = document.querySelector('.sl-2');
+const learnMoreBtn3 = document.querySelector('.sl-3');
+const servDescrip1 = document.querySelector('.service-description-1');
+const servDescrip2 = document.querySelector('.service-description-2');
+const servDescrip3 = document.querySelector('.service-description-3');
 
 //drop down menu for navigation on smaller screens
 menuIcon.addEventListener('click', () => {
@@ -16,6 +20,19 @@ menuIcon.addEventListener('click', () => {
 //scroll down animation from hero button to review section
 scrollDownBtn.addEventListener('click', () => {
   reviewSection.scrollIntoView({behavior: "smooth"});
+});
+
+//learn more button code to drop down description
+learnMoreBtn1.addEventListener('click', () => {
+  servDescrip1.classList.remove('hide');
+});
+
+learnMoreBtn2.addEventListener('click', () => {
+  servDescrip2.classList.remove('hide');
+});
+
+learnMoreBtn3.addEventListener('click', () => {
+  servDescrip3.classList.remove('hide');
 });
 
 //carousel function
