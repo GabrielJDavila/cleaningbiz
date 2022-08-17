9ce2a73b4ca262bd3b20af6a4a6c8feb16253604
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2022-08-17 14:22:18 -0500

    Add files via upload
    added a review carousel

--- js/site.js
+++ js/site.js
@@ -4,8 +4,12 @@ const menuIcon = document.querySelector('.material-symbols-outlined');
 const dropDownMenu = document.querySelector('nav')
 const scrollDownBtn = document.querySelector('.scroll-down')
 const reviewSection = document.querySelector('.reviews-section')
-
-
+const learnMoreBtn1 = document.querySelector('.sl-1');
+const learnMoreBtn2 = document.querySelector('.sl-2');
+const learnMoreBtn3 = document.querySelector('.sl-3');
+const servDescrip1 = document.querySelector('.service-description-1');
+const servDescrip2 = document.querySelector('.service-description-2');
+const servDescrip3 = document.querySelector('.service-description-3');
 
 //drop down menu for navigation on smaller screens
 menuIcon.addEventListener('click', () => {
@@ -16,6 +20,19 @@ menuIcon.addEventListener('click', () => {
 //scroll down animation from hero button to review section
 scrollDownBtn.addEventListener('click', () => {
   reviewSection.scrollIntoView({behavior: "smooth"});
+});
+
+//learn more button code to drop down description
+learnMoreBtn1.addEventListener('click', () => {
+  servDescrip1.classList.remove('hide');
+});
+
+learnMoreBtn2.addEventListener('click', () => {
+  servDescrip2.classList.remove('hide');
+});
+
+learnMoreBtn3.addEventListener('click', () => {
+  servDescrip3.classList.remove('hide');
 });
 
 //carousel function
@@ -45,5 +62,66 @@ document.querySelectorAll(".carousel").forEach(carousel => {
   });
 
   items[0].classList.add("carousel-item-selected");
-  button[0].classList.add("carousel-button-selected");
+  buttons[0].classList.add("carousel-button-selected");
+});
+
+
+//review carousel
+const reviews = [
+  {
+    id: 1,
+    name: "Aurora Dentrix",
+    job: "Office Cleaning",
+    img: "./img/cleaning3.jpg",
+    text: "We have to look our best for our clients, and the office space needed a deep clean. We now use them monthly to save us the time and focus on our best work."
+  },
+  {
+    id: 2,
+    name: "Kelly Williams, Realtor",
+    job: "Move Out Cleaning",
+    img: "./img/cleaning4.jpg",
+    text: "They're the best! Helped me clean my units for showings."
+  },
+  {
+    id: 3,
+    name: "Edge Fitness",
+    job: "Gym Cleaning",
+    img: "./img/cleaning6.jpg",
+    text: "They are proffessionals and get the job done perfectly. We use them on a weekly basis for deep cleans of the facility."
+  }
+]
+
+// select items
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('service-reviewed');
+const info = document.getElementById('review-info');
+
+const prevBtn = document.querySelector('.prev-btn');
+const nxtBtn = document.querySelector('.next-btn');
+let currentItem = 0;
+
+//load initial items
+window.addEventListener("DOMContentLoaded", function () {
+  showReview(currentItem)
+});
+
+// show review based on item
+function showReview(person) {
+  const item = reviews[person];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+};
+
+// show next review
+nxtBtn.addEventListener('click', () => {
+  currentItem++;
+  showReview(currentItem);
+});
+
+prevBtn.addEventListener('click', () => {
+  currentItem --;
+  showReview(currentItem);
 });
