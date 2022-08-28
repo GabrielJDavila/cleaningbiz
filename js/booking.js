f4daf66e347a014cd5bd77b89d584c32491bbfc9
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2022-08-28 14:50:47 -0500

    Add files via upload
    Added more features to booking form.

--- js/booking.js
+++ js/booking.js
@@ -2,9 +2,9 @@ console.log("helloooo");
 const menuIcon = document.querySelector('.material-symbols-outlined');
 const dropDownMenu = document.querySelector('nav')
 const residentialBtn = document.getElementById('residential');
-const commercialBtn = document.getElementById('commercial');
+const moveBtn = document.getElementById('movein-moveout');
 const residentialDiv = document.querySelector('.residential-cleaning');
-const commercialDiv = document.querySelector('.commercial-cleaning');
+const moveDiv = document.querySelector('.move-cleaning');
 const submitBtn = document.querySelector('.submit-btn');
 
 //drop down menu for navigation on smaller screens
@@ -16,22 +16,41 @@ menuIcon.addEventListener('click', () => {
 //functions for showing residential fill-in info
 residentialBtn.addEventListener('click', () => {
   residentialDiv.classList.remove('hide');
-  commercialDiv.classList.add('hide');
+  moveDiv.classList.add('hide');
 });
 
 
 //function for showing commercial fill-in info
-commercialBtn.addEventListener('click', () => {
-  commercialDiv.classList.remove('hide');
+moveBtn.addEventListener('click', () => {
+  moveDiv.classList.remove('hide');
   residentialDiv.classList.add('hide');
   //js for booking page calculation animation
-  const comEstimateBtn = document.querySelector('.com-estimate-btn');
-  comEstimateBtn.addEventListener('click', () => {
+  const moveEstimateBtn = document.querySelector('.move-estimate-btn');
+  moveEstimateBtn.addEventListener('click', () => {
+    const genClean = document.querySelector('.gen-clean');
+    const deepClean = document.querySelector('.deep-clean');
     const squareFt = document.getElementById('squareft');
-    const comCost = document.querySelector('.com-cost');
-    let squareFtValue = squareFt.value * .20;
-    comCost.textContent = squareFtValue;
-  })
+    const moveCost = document.querySelector('.move-cost');
+    const moveExtras1 = document.querySelector('.extras1');
+    const moveExtras2 = document.querySelector('.extras2');
+    const moveExtras3 = document.querySelector('.extras3');
+    const wkClean = document.querySelector('.weekly');
+    const bwkClean = document.getElementById('bi-weekly');
+    const mClean = document.getElementById('monthly');
+
+    if (deepClean.selected && moveExtras1.checked && moveExtras2.checked && moveExtras3.checked && wkClean.checked) {
+      let squareFtValue = Math.floor((squareFt.value * .20) + 45);
+      moveCost.textContent = squareFtValue;
+      alert("This works!");
+    } else if (deepClean.selected && moveExtras1.checked && moveExtras2.checked && moveExtras3.checked == false || deepClean.selected == true && moveExtras1.checked && moveExtras2.checked == false && moveExtras3.checked || deepClean.selected == true && moveExtras1.checked == false && moveExtras2.checked && moveExtras3.checked) {
+        let squareFtValue = Math.floor((squareFt.value * .20) + 30);
+        moveCost.textContent = squareFtValue;
+    } else if (deepClean.selected && moveExtras1.checked && moveExtras2.checked == false && moveExtras3.checked == false || deepClean.selected && moveExtras1.checked == false && moveExtras2.checked && moveExtras3.checked == false || deepClean.selected && moveExtras1.checked == false && moveExtras2.checked == false && moveExtras3.checked) {
+        let squareFtValue = Math.floor((squareFt.value * .20) + 15);
+        moveCost.textContent = squareFtValue;
+    };
+
+  });
 
   submitBtn.addEventListener('click', () => {
     console.log("hiiii");
